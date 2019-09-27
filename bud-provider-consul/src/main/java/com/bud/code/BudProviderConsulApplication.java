36326e1118ed8f5ac40a03c46162b70732c9ae6b
kunan01 <kunan01@163.com>
2019-09-27 11:46:53 +0800

    提交代码

---
+++ bud-provider-consul/src/main/java/com/bud/code/BudProviderConsulApplication.java
@@ -0,0 +1,27 @@
+package com.bud.code;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BudProviderConsulApplication implements CommandLineRunner {
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudProviderConsulApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("BudProviderConsulApplication.....启动成功。"+name);
+    }
+
+
+
+}
