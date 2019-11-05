07d6a327e6f9c3e217a49d736b3f7e517a50da20
kunan01 <kunan01@163.com>
2019-11-05 14:57:08 +0800

    提交代码

--- bud-provider-consul/src/main/java/com/bud/code/BudProviderConsulApplication.java
+++ bud-provider-consul/src/main/java/com/bud/code/BudProviderConsulApplication.java
@@ -1,11 +1,13 @@
 package com.bud.code;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan({"com.bud.code.mapper.*"})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BudProviderConsulApplication implements CommandLineRunner {
--- bud-provider-consul/src/test/java/com/bud/code/BudProviderConsulApplicationTests.java
+++ bud-provider-consul/src/test/java/com/bud/code/BudProviderConsulApplicationTests.java
@@ -11,6 +11,7 @@ public class BudProviderConsulApplicationTests {
 
     @Test
     public void contextLoads() {
+        
     }
 
 }
